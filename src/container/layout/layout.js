2d4fa3f2c1392decfe74944ebf31422e757c2046
Usman <usmansiddiq000@gmail.com>
2019-02-19 17:46:42 +0500

    adding bootstrap

---
+++ src/container/layout/layout.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react';
+
+import classes from './layout.css';
+
+class Layout extends Component {
+    render(){
+        return (
+            <div className = {classes.layout}>This is New layout</div>
+        )
+    }
+}
+
+export default Layout;
